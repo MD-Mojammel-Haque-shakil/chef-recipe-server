a35c64517bfb0ebc8e5dfe8f5a406a13d0ced3ea
MD-Mojammel-Haque-Shakil <mojammelhaque8967@gmail.com>
2023-05-03 12:16:58 +0600

    install cors and use cors in index.js file and send chef data done

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require ('express');
 const app = express();
+const cors = require('cors')
 const port = process.env.PORT || 5000;
+
+const chef = require ('./data/chef.json');
+app.use(cors());
 
 app.get('/', (req, res)=>{
     res.send('chef is comming')
+});
+
+app.get('/chef', (req, res)=>{
+    res.send(chef);
 });
 
 app.listen(port, ()=>{
